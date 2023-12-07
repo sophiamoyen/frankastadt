8c8db2f3a01de4e2c7839a91be2b486be3a3bfef
root <marvin.heidinger@t-online.de>
2023-12-07 13:25:32 +0000

    add simple integration of pc and grasp

--- fs_pick_and_place/scripts/pick_box.py
+++ fs_pick_and_place/scripts/pick_box.py
@@ -5,10 +5,10 @@ from pick_and_place_module.pick_and_place import PickAndPlace
 
 def task():
     pick_and_place = PickAndPlace(0.05, 0.5)
-    pick_position=[rospy.get_param("cube_0_x"),rospy.get_param("cube_0_y"),rospy.get_param("cube_0_z")+0.05]
+    pick_position=[rospy.get_param("cube_x"),rospy.get_param("cube_y"),rospy.get_param("cube_z")+0.05]
     pick_orientation=[1.57,3.14,0]
 
-    place_position=[rospy.get_param("cube_0_x")+0.05,rospy.get_param("cube_0_y")+0.2,rospy.get_param("cube_0_z")+0.1]
+    place_position=[rospy.get_param("cube_x")+0.05,rospy.get_param("cube_y")+0.2,rospy.get_param("cube_z")+0.1]
     place_orientation=[1.57,3.14,0]
     
     pick_and_place.setPickPose(*pick_position,*pick_orientation)
