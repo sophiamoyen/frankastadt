d84ebc686caaa3ce7bcd58b6b5bce57185b60130
root <sophiamoyen@gmail.com>
2024-03-14 16:36:42 +0000

    Changes to 3 cube tower test Marvin

--- fs_pick_and_place/scripts/run_tower.py
+++ fs_pick_and_place/scripts/run_tower.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
                         cube_positions,
                         cube_indexes)
 
-    closest_cube, closest_cube_index = cla.find_closest_cube(cube_positions, cube_indexes)
+    closest_cube, closest_cube_index = cla.find_closest_cube(cube_positions, desired_center, cube_indexes)
     print("========== Removed cube_{} from occupied space".format(closest_cube_index))
 
 
@@ -67,11 +67,11 @@ if __name__ == "__main__":
 
     # Getting Pick  SIMULATION
     pick_positions = [[*closest_cube,rospy.get_param("cube_0_z")]]
-    tower_closest_cube, closest_cube_index = cla.find_closest_cube(cubes_to_move, cubes_to_move_indexes, closest_cube)
+    tower_closest_cube, closest_cube_index = cla.find_closest_cube(cubes_to_move, closest_cube, cubes_to_move_indexes)
     pick_positions.append([*tower_closest_cube,rospy.get_param("cube_0_z")])
     cubes_to_move_indexes.remove(closest_cube_index)
     cubes_to_move.remove(tower_closest_cube)
-    tower_closest_cube, closest_cube_index = cla.find_closest_cube(cubes_to_move, cubes_to_move_indexes, closest_cube)
+    tower_closest_cube, closest_cube_index = cla.find_closest_cube(cubes_to_move, closest_cube, cubes_to_move_indexes)
     pick_positions.append([*tower_closest_cube,rospy.get_param("cube_0_z")])
 
     print("=========== Place positions:",cubes_tower_pos)
